b80accf1f85b73b29e0c319bb2f8973cea2a2d86
DHRUVI5674 <dhruvipatel5674@gmail.com>
2026-04-23 00:19:22 +0530

    feat: add create multiple notes endpoint (POST /api/notes/multiple)

--- src/routes/note.routes.js
+++ src/routes/note.routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const { createNote } = require('../controllers/note.controller.js');
+const { createNote,createMultiple } = require('../controllers/note.controller.js');
 
 router.post('/', createNote);
+router.post('/multiple',createMultiple);
 
 module.exports = router;
